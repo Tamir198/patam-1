9bbfe59f5c7458d78a37eda3e3a3dfb7fc85e062
Tamir <1tamir198@gmail.com>
2021-11-01 23:08:09 +0200

    Adding csv file reader

--- src/test/TimeSeries.java
+++ src/test/TimeSeries.java
@@ -1,8 +1,25 @@
 package test;
 
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class TimeSeries {
-	public TimeSeries(String csvFileName) {
-	}
-	
+    private String fileName;
+
+    public TimeSeries(String csvFileName) {
+        this.fileName = csvFileName;
+    }
+
+    public void readcvsFile() throws Exception {
+        String splitBy = ",";
+        BufferedReader br = new BufferedReader(new FileReader(this.fileName));
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] b = line.split(splitBy);
+            System.out.println(b[0]);
+        }
+        br.close();
+    }
+
 }
