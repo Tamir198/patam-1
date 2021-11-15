7eeef3da82079bfda9ba873bb11d1119e85a890f
Tamir <1tamir198@gmail.com>
2021-11-15 17:56:13 +0200

    Code clean

--- src/test/TimeSeries.java
+++ src/test/TimeSeries.java
@@ -3,61 +3,54 @@ package test;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.Arrays;
 
 
 public class TimeSeries {
 
-    String fileName;
-    float[][] dataMatrix;
-    String[] criteriaTitles;
+    private String fileName;
+    private float[][] dataMatrix;
+    private String[] criteriaTitles;
 
 
     public TimeSeries(String csvFileName) {
         fileName = csvFileName;
     }
 
-    public void readCsvFile() throws Exception {
-        int numOfCriteria = 0;
-        int counter = 1; // represents the lines in the matrix dataNames
+    void readCsvFile() throws Exception {
+        int numOfCriteria = 0, counter = 0;
         String[] data;
-        String delimiter = ",";
-        BufferedReader buf = new BufferedReader(new FileReader((this.fileName)));
-        String line;
+        String delimiter = ",", line;
+        BufferedReader buf = new BufferedReader(new FileReader(this.fileName));
 
         line = buf.readLine();
-        if (line!= null){
+        if (line != null) {
             data = line.split(delimiter);
             numOfCriteria = data.length;
             dataMatrix = new float[getNumOfLines(fileName)][numOfCriteria];
-            criteriaTitles  = new String[data.length];
-            for (int i = 0; i < data.length ; i++) {
-                criteriaTitles[i] = data[i];
-            }
+            criteriaTitles = new String[data.length];
+            System.arraycopy(data, 0, criteriaTitles, 0, data.length);
         }
 
-
-        while((line = buf.readLine()) != null){
+        while ((line = buf.readLine()) != null) {
             data = line.split(delimiter);
 
-            for (int i=0; i<numOfCriteria; i++){
-                dataMatrix[counter][i]  = Float.parseFloat((data[i]));
+            for (int i = 0; i < numOfCriteria; i++) {
+                dataMatrix[counter][i] = Float.parseFloat(data[i]);
             }
-
-            System.out.println(Arrays.toString(data));
             counter++;
         }
 
         buf.close();
     }
 
-    public int getNumOfLines (String fileName) throws Exception{
+    public int getNumOfLines(String fileName) throws Exception {
         BufferedReader buf = new BufferedReader(new FileReader((fileName)));
         int counter = 0; // the function will return one line more than reality
-        while((buf.readLine()) != null){
+
+        while ((buf.readLine()) != null) {
             counter++;
         }
-        return counter;
+        return counter - 1;
     }
 
 }
